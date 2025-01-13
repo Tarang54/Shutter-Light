8f9b9d0c5a6686515b729dab45dc302194cf2a77
Tarang <tarang.sultania@classplus.co>
2025-01-13 15:54:53 +0530

    integrated tailwind and added components

---
+++ src/sections/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div className="navs">
+        <a href="/"><li>Home</li></a>
+        <a href="/"><li>Services</li></a>
+        <a href="/"><li>About Us</li></a>
+        <a href="/"><li>Portfolio</li></a>
+        <a href="/"><li>Contacts</li></a>
+      </div>
+      <div className="lang">EN</div>
+    </div>
+  )
+}
+
+export default Header
